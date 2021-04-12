f8651735375b7bf07becd9bb900c258df50d4920
Chuanyu Xiao <cxiao9@ucsc.edu>
2021-04-12 09:28:29 -0700

    fixed the game over scene, wrong key indicated

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -67,7 +67,7 @@ class Play extends Phaser.Scene{
           scoreConfig.fixedWidth = 0;
           this.clock = this.time.delayedCall(60000, ()=>{
             this.add.text(game.config.width / 2, game.config.height / 2, 'GAME OVER', scoreConfig).setOrigin(0.5);
-            this.add.text(game.config.width / 2, game.config.height / 2 + 64, '(F)ire to Restart', 
+            this.add.text(game.config.width / 2, game.config.height / 2 + 64, '(R) to Restart', 
             scoreConfig).setOrigin(0.5);
             this.gameOver = true;
           }, null, this);
